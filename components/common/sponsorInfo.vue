af80f142fbe1c6346857d5773340a08a9067b39c
practicatto <practicatto@gmail.com>
2024-10-31 23:02:34 -0500

    add award icon and fix networking typo

--- components/common/sponsorInfo.vue
+++ components/common/sponsorInfo.vue
@@ -1,49 +1,24 @@
 <template>
   <v-container fluid class="pa-0 ma-0">
-    <v-row
-      v-for="(item, index) in sponsorsData"
-      :key="index"
-      class="google-font mb-5 mt-0 justify-center"
-    >
+    <v-row v-for="(item, index) in sponsorsData" :key="index" class="google-font mb-5 mt-0 justify-center">
       <v-col md="12" cols="12" class="mb-n1">
         <h2 class="d-flex align-center justify-center">
-          <img
-            v-if="item.category_icon"
-            :src="'/img/common/' + item.category_icon"
-            width="50px"
-            style="padding-top: 12px"
-          />
+          <Award v-if="item.category_color" :pathColor="item.category_color" :circleColor="item.category_color" 
+            :strokeWidth="0.6" />
           <span>
             {{ item.category_name }}
           </span>
-          <img
-            v-if="item.category_icon"
-            :src="'/img/common/' + item.category_icon"
-            width="50px"
-            style="padding-top: 12px"
-          />
+          <Award v-if="item.category_color" :pathColor="item.category_color" :circleColor="item.category_color"
+            :strokeWidth="0.6" />
         </h2>
       </v-col>
-      <v-col
-        cols="6"
-        sm="4"
-        md="3"
-        class="text-center"
-        style="min-height: 150px;"
-        v-for="(sponsor, indexp) in item.sponsors"
-        :key="indexp"
-      >
+      <v-col cols="6" sm="4" md="3" class="text-center" style="min-height: 150px;"
+        v-for="(sponsor, indexp) in item.sponsors" :key="indexp">
         <div class="pa-5 sponsor-container">
           <ClientOnly>
             <v-tooltip location="bottom" :key="indexp">
               <template v-slot:activator="{ props }">
-                <a
-                  v-bind="props"
-                  target="_blank"
-                  class="w-100"
-                  :href="sponsor.link"
-                  :aria-label="sponsor.name"
-                >
+                <a v-bind="props" target="_blank" class="w-100" :href="sponsor.link" :aria-label="sponsor.name">
                   <v-img alt="sponsor-logo" :src="'/img/sponsors/' + sponsor.logo"></v-img>
                 </a>
               </template>
@@ -57,6 +32,8 @@
 </template>
 
 <script setup>
+import Award from '~/components/icons/Award.vue';
+
 const { sponsorsData } = useJSONData();
 </script>
 
